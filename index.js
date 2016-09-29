dba25c7bf4f47b7e93529037c32ec8cfc562b8ac
wrc126 <84104406@qq.com>
2016-09-29 20:28:26 +0800

    test

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 var app = require('express')(),
-conf =require('./lib/config'),
-
-wechat=require('./lib/wechat'),
+    conf = require('./lib/config'),
+    wechat = require('./lib/wechat'),
     bodyParser = require('body-parser');
 
-    app.use(bodyParser.urlencoded({
-      extended: false
-    }));
+    app.use(bodyParser.urlencoded({extended: false}));
 
-wechat(conf.wechat);
-wechat.createMenu(require('./lib/menu.json'));
-
+    wechat(conf.wechat);
+    wechat.createMenu(require('./lib/menu.json'));
 
     app.use('/wxapi',require('./lib/routers/wxapi.js'));
 
     app.listen(8014,function(err){
       console.log('listenning at 8014...');
-    })
+    });
